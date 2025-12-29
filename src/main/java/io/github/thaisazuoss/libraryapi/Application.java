49cff180a8ae638258bc7238005d8b1b871ae24a
ThaisAzuos <thais.azuoss@gmail.com>
2025-12-29 13:20:10 -0300

    Adicionando auditoria JPA nas Entidades

--- src/main/java/io/github/thaisazuoss/libraryapi/Application.java
+++ src/main/java/io/github/thaisazuoss/libraryapi/Application.java
@@ -4,10 +4,12 @@ import io.github.thaisazuoss.libraryapi.model.Autor;
 import io.github.thaisazuoss.libraryapi.repository.AutorRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import java.time.LocalDate;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class Application {
 
 	public static void main(String[] args) {
