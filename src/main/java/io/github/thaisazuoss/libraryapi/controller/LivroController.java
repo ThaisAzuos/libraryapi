1e931390a7b266150bf436166c08036cf7de7a5f
ThaisAzuos <thais.azuoss@gmail.com>
2026-01-07 17:10:12 -0300

    Cadastro de livro com bean validation

--- src/main/java/io/github/thaisazuoss/libraryapi/controller/LivroController.java
+++ src/main/java/io/github/thaisazuoss/libraryapi/controller/LivroController.java
@@ -1,7 +1,14 @@
 package io.github.thaisazuoss.libraryapi.controller;
 
+import io.github.thaisazuoss.libraryapi.controller.dto.request.LivroRequestDTO;
+import io.github.thaisazuoss.libraryapi.controller.dto.response.ErroResposta;
+import io.github.thaisazuoss.libraryapi.exceptions.RegistroDuplicadoException;
 import io.github.thaisazuoss.libraryapi.service.LivroService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +18,22 @@ import org.springframework.web.bind.annotation.RestController;
 public class LivroController {
 
     private final LivroService livroService;
+
+    @PostMapping
+    public ResponseEntity<?> salvar(@RequestBody @Valid LivroRequestDTO livroRequestDTO){
+        try {
+            //MAPEAR DTO PARA ENTIDADE
+
+            //ENVIAR A ENTIDADE PARA O SERVICE VALIDAR E SALVAR NA BASE
+
+            //CRIAR URL PASRA ACESSO DOS DADOS DO LIVRO
+
+            //RETORNAR CODIGO CREATED COM HEADER LOCATION
+
+            return ResponseEntity.ok(livroRequestDTO);
+        }catch (RegistroDuplicadoException e){
+            var erroDto = ErroResposta.respostaConflito(e.getMessage());
+            return ResponseEntity.status(erroDto.status()).body(erroDto);
+        }
+    }
 }
--- src/main/java/io/github/thaisazuoss/libraryapi/controller/dto/request/LivroRequestDTO.java
+++ src/main/java/io/github/thaisazuoss/libraryapi/controller/dto/request/LivroRequestDTO.java
@@ -1,16 +1,30 @@
 package io.github.thaisazuoss.libraryapi.controller.dto.request;
 
 import io.github.thaisazuoss.libraryapi.model.GeneroLivro;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import org.hibernate.validator.constraints.ISBN;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.UUID;
 
 public record LivroRequestDTO(
+        @ISBN
+        @NotBlank(message = "campo obrigatório")
         String isbn,
+
+        @NotBlank(message = "campo obrigatório")
         String titulo,
+
+        @NotNull(message = "campo obrigatório")
+        @Past(message = "Não pode ser uma data futura")
         LocalDate dataPublicacao,
+
         GeneroLivro genero,
         BigDecimal preco,
+
+        @NotNull(message = "campo obrigatório")
         UUID idAutor) {
 }
