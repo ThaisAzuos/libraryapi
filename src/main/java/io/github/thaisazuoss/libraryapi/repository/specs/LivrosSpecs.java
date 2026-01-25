e4cc2ac691e26afbb141f3022008fffd08f3da83
ThaisAzuos <thais.azuoss@gmail.com>
2026-01-25 00:09:32 -0300

    Implementando Specifications da entidade Livro

---
+++ src/main/java/io/github/thaisazuoss/libraryapi/repository/specs/LivrosSpecs.java
@@ -0,0 +1,23 @@
+package io.github.thaisazuoss.libraryapi.repository.specs;
+
+import io.github.thaisazuoss.libraryapi.model.GeneroLivro;
+import io.github.thaisazuoss.libraryapi.model.Livro;
+import org.springframework.data.jpa.domain.Specification;
+
+public class LivrosSpecs {
+
+    public static Specification<Livro> isbnEqual(String isbn){
+        return (root, query, criteriaBuilder) ->
+                criteriaBuilder.equal(root.get("isbn"), isbn);
+    }
+
+    public static Specification<Livro> tituloLike(String titulo){
+        return (root, query, criteriaBuilder) ->
+                criteriaBuilder.like(criteriaBuilder.upper(root.get("titulo")) , "%" + titulo.toUpperCase() + "%");
+    }
+
+    public static Specification<Livro> generoEqual(GeneroLivro genero){
+        return (root, query, criteriaBuilder) ->
+                criteriaBuilder.equal(root.get("genero"), genero);
+    }
+}
--- src/main/java/io/github/thaisazuoss/libraryapi/service/LivroService.java
+++ src/main/java/io/github/thaisazuoss/libraryapi/service/LivroService.java
@@ -1,12 +1,18 @@
 package io.github.thaisazuoss.libraryapi.service;
 
+import io.github.thaisazuoss.libraryapi.model.GeneroLivro;
 import io.github.thaisazuoss.libraryapi.model.Livro;
 import io.github.thaisazuoss.libraryapi.repository.LivroRepository;
+import io.github.thaisazuoss.libraryapi.repository.specs.LivrosSpecs;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import static io.github.thaisazuoss.libraryapi.repository.specs.LivrosSpecs.*;
 
 @Service
 @RequiredArgsConstructor
@@ -24,5 +30,27 @@ public class LivroService {
 
     public void deletar(Livro livroDeletado) {
         livroRepository.delete(livroDeletado);
+    }
+
+    public List<Livro> pesquisar(String isbn, String titulo, String nomeAutor, GeneroLivro genero, int anoPublicacao){
+
+//        Specification<Livro> specs = Specification
+//                .where(LivrosSpecs.isbnEqual(isbn)
+//                        .and(LivrosSpecs.tituloLike(titulo))
+//                        .and(LivrosSpecs.generoEqual(genero)));
+
+        Specification<Livro> specs = Specification
+                .where((root, query, criteriaBuilder) -> criteriaBuilder.conjunction());
+        if (isbn != null){
+            specs = specs.and(isbnEqual(isbn));
+        }
+        if (titulo != null){
+            specs = specs.and(tituloLike(titulo));
+        }
+        if (genero != null){
+            specs = specs.and(generoEqual(genero));
+        }
+
+        return livroRepository.findAll(specs);
     }
 }
